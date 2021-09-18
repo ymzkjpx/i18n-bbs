2a6b06a87c47b5440f7c112c46446f8765f134b7
ymzkjpx <ymzkjpx@gmail.com>
2021-09-18 21:22:19 +0900

    ディレクトリを移動

---
+++ project/src/test/java/ex/i18nbbs/I18nBbsApplicationTests.java
@@ -0,0 +1,13 @@
+package ex.i18nbbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class I18nBbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
