62ceb1c15b53d2797f42c3e1a3a1335ab6441e6b
ymzkjpx <ymzkjpx@gmail.com>
2021-09-18 16:53:02 +0900

    first commit

---
+++ src/main/java/ex/i18nbbs/I18nBbsApplication.java
@@ -0,0 +1,13 @@
+package ex.i18nbbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I18nBbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(I18nBbsApplication.class, args);
+    }
+
+}
